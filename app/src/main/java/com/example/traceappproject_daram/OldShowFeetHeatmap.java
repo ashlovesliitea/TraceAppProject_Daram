54e3ffa497b859e76123eefbd379e174f03e26c0
rhalwls <coding3392@gmail.com>
2021-01-26 16:39:37 +0900

    (x-13)add arch and back

--- app/src/main/java/com/example/traceappproject_daram/OldShowFeetHeatmap.java
+++ app/src/main/java/com/example/traceappproject_daram/OldShowFeetHeatmap.java
@@ -2,59 +2,58 @@ package com.example.traceappproject_daram;
 
 import android.graphics.Color;
 import android.os.AsyncTask;
+import androidx.annotation.AnyThread;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
-import androidx.annotation.AnyThread;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.collection.ArrayMap;
-
 import java.util.Map;
 import java.util.Random;
 
+import androidx.collection.ArrayMap;
 import ca.hss.heatmaplib.HeatMap;
 import ca.hss.heatmaplib.HeatMapMarkerCallback;
 
-public class HeatMapFeet extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class OldShowFeetHeatmap extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
     //한번은 화면 터치해야 점들이 나와요
     //지금은 터치할 때마다 점들이 리셋돼요
     private HeatMapHolder map;
     private boolean testAsync = false;
 
-    private static final String TAG = "ShowFeetHeatmap";
-
+    private static final String TAG ="ShowFeetHeatmap";
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.x13);
+        setContentView(R.layout.activity_show_feet_heatmap);
 
-        //CheckBox box = findViewById(R.id.change_async_status);
-        //box.setOnCheckedChangeListener(this);
+        CheckBox box = findViewById(R.id.change_async_status);
+        box.setOnCheckedChangeListener(this);
 
-        map = (HeatMapHolder) findViewById(R.id.feetmap);//down casting이 되지 않음
+        map = (HeatMapHolder)findViewById(R.id.example_map);//down casting이 되지 않음
         //map.setIdResource(R.mipmap.face);//이러면 ondraw한 다음이라 안되지
         map.setMinimum(0.0);
-        map.setMaximum(9.0);//강도의 최대값은 얼마냐
-        map.setLeftPadding(0);
-        map.setRightPadding(0);
-        map.setTopPadding(0);
-        map.setBottomPadding(0);
-        //marker 색깔 바꿀 수 있음 0xff9400D3
-        map.setMarkerCallback(new HeatMapMarkerCallback.CircleHeatMapMarker(0x00000000));
+        map.setMaximum(100.0);
+        map.setLeftPadding(100);
+        map.setRightPadding(100);
+        map.setTopPadding(100);
+        map.setBottomPadding(100);
+        map.setMarkerCallback(new HeatMapMarkerCallback.CircleHeatMapMarker(0xff9400D3));
         map.setRadius(80.0);
         //map.setBackgroundResource(R.mipmap.ic_launcher);//걍 이 이미지만 뜸 ㅠㅠ 벡터라 빈 곳은 맵이 비칠줄알았는데
         //map.setForeground(getDrawable(R.mipmap.ic_launcher));//위에 코드랑 똑같음 그리고 이 함수는 view에 속해있는 거임..
         //canvas사용했을때
 
+
         Map<Float, Integer> colors = new ArrayMap<>();
         //build a color gradient in HSV from red at the center to green at the outside
         for (int i = 0; i < 21; i++) {
-            float stop = ((float) i) / 20.0f;
-            int color = doGradient(i * 5, 0, 100, 0xff0000ff, 0xffff0000);
+            float stop = ((float)i) / 20.0f;
+            int color = doGradient(i * 5, 0, 100, 0xff00ff00, 0xffff0000);
             colors.put(stop, color);
         }
         map.setColorStops(colors);
@@ -63,6 +62,7 @@ public class HeatMapFeet extends AppCompatActivity implements CompoundButton.OnC
             @Override
             public void onMapClicked(int x, int y, HeatMap.DataPoint closest) {
                 addData();
+                //addOverlayImage();
             }
         });
 
@@ -84,7 +84,8 @@ public class HeatMapFeet extends AppCompatActivity implements CompoundButton.OnC
                     });
                 }
             });
-        } else {
+        }
+        else {
             drawNewMap();
             map.forceRefresh();
         }
@@ -124,17 +125,14 @@ public class HeatMapFeet extends AppCompatActivity implements CompoundButton.OnC
         point = new HeatMap.DataPoint(c1, c2, c3);
         map.addData(point);
          */
-        Feet left = new Feet(false);
-        Feet right = new Feet(true);
-        passFeetToHeatMap(left, map);
-        passFeetToHeatMap(right,map);
+        FootOneFrame footOneFrame = new FootOneFrame(false);
+        passFeetToHeatMap(footOneFrame, map);
     }
-
-    private void passFeetToHeatMap(Feet feet, HeatMapHolder map) {
-        for (int i = 0; i < Feet.SENSOR_NUM; i++) {
-            float c1 = feet.ratioW[i];
-            float c2 = feet.ratioH[i];
-            double c3 = feet.ps[i];
+    private void passFeetToHeatMap(FootOneFrame footOneFrame, HeatMapHolder map){
+        for(int i = 0; i< FootOneFrame.SENSOR_NUM; i++){
+            float c1 = footOneFrame.ratioW[i];
+            float c2 = footOneFrame.ratioH[i];
+            double c3 = footOneFrame.ps[i];
             HeatMap.DataPoint point = new HeatMap.DataPoint(c1, c2, c3);
             map.addData(point);
         }
@@ -160,7 +158,7 @@ public class HeatMapFeet extends AppCompatActivity implements CompoundButton.OnC
         }
         float[] hsvmin = new float[3];
         float[] hsvmax = new float[3];
-        float frac = (float) ((value - min) / (max - min));
+        float frac = (float)((value - min) / (max - min));
         Color.RGBToHSV(Color.red(min_color), Color.green(min_color), Color.blue(min_color), hsvmin);
         Color.RGBToHSV(Color.red(max_color), Color.green(max_color), Color.blue(max_color), hsvmax);
         float[] retval = new float[3];
