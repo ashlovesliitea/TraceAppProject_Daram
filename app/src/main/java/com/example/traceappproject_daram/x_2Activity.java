9ff833f48df6d119166a227e3360da377ee6a74f
min <12184892@inha.edu>
2021-02-02 18:05:01 +0900

    x13 xml x2 activity 수정

--- app/src/main/java/com/example/traceappproject_daram/x_2Activity.java
+++ app/src/main/java/com/example/traceappproject_daram/x_2Activity.java
@@ -1,4 +1,4 @@
-package com.example.traceappproject_daram;
+package com.example.experiment;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +23,7 @@ import com.android.volley.Response;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class x_2Activity extends AppCompatActivity {
 
@@ -29,17 +31,19 @@ public class x_2Activity extends AppCompatActivity {
     private EditText login_email, login_password;
     private Button button2;
     private Button button3;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState );
-        setContentView( R.layout.x_2);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.x_2);
+
 
         login_email = findViewById(R.id.login_email);
         login_password = findViewById(R.id.login_password);
 
-        button2 = findViewById( R.id.button2 );
-        button2.setOnClickListener( new View.OnClickListener() {
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String UserEmail = login_email.getText().toString();
@@ -49,26 +53,26 @@ public class x_2Activity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            JSONObject jsonObject = new JSONObject( response );
-                            boolean success = jsonObject.getBoolean( "success" );
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
 
-                            if(success) {//로그인 성공시
+                            if (success) {//로그인 성공시
 
-                                String UserEmail = jsonObject.getString( "UserEmail" );
-                                String UserPwd = jsonObject.getString( "UserPwd" );
-                                String UserName = jsonObject.getString( "UserName" );
+                                String UserEmail = jsonObject.getString("UserEmail");
+                                String UserPwd = jsonObject.getString("UserPwd");
+                                String UserName = jsonObject.getString("UserName");
 
-                                Toast.makeText( getApplicationContext(), String.format("%s님 환영합니다.", UserName), Toast.LENGTH_SHORT ).show();
-                                Intent intent = new Intent(x_2Activity.this,x_15Activity.class );
+                                Toast.makeText(getApplicationContext(), String.format("%s님 환영합니다.", UserName), Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(x_2Activity.this, x_15Activity.class);
 
-                                intent.putExtra( "UserEmail", UserEmail );
-                                intent.putExtra( "UserPwd", UserPwd );
-                                intent.putExtra( "UserName", UserName );
+                                intent.putExtra("UserEmail", UserEmail);
+                                intent.putExtra("UserPwd", UserPwd);
+                                intent.putExtra("UserName", UserName);
 
-                                startActivity( intent );
+                                startActivity(intent);
 
                             } else {//로그인 실패시
-                                Toast.makeText( getApplicationContext(), "로그인에 실패하셨습니다.", Toast.LENGTH_SHORT ).show();
+                                Toast.makeText(getApplicationContext(), "로그인에 실패하셨습니다.", Toast.LENGTH_SHORT).show();
                             }
 
                         } catch (JSONException e) {
@@ -76,9 +80,19 @@ public class x_2Activity extends AppCompatActivity {
                         }
                     }
                 };
-               // LoginRequest loginRequest = new LoginRequest(UserEmail, UserPwd, responseListener );
+                // LoginRequest loginRequest = new LoginRequest(UserEmail, UserPwd, responseListener );
                 //RequestQueue queue = Volley.newRequestQueue( x_2Activity.this );
                 //queue.add( loginRequest );//서버연동내용이었네요
+
+            }
+        });
+
+        text = findViewById(R.id.text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(x_2Activity.this, x_15Activity.class);
+                startActivity(intent);
 
             }
         });
