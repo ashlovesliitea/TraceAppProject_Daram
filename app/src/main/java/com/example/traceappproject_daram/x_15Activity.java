0a9718e21d75cc043e07ef8eb0f8d09b69ef951a
min <12184892@inha.edu>
2021-02-09 10:36:18 +0900

    spinner added

--- app/src/main/java/com/example/traceappproject_daram/x_15Activity.java
+++ app/src/main/java/com/example/traceappproject_daram/x_15Activity.java
@@ -9,13 +9,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class x_15Activity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.x_15_1);
+        Spinner spinner = findViewById(R.id.spinner);
+
+        String[] items = new String[]{"010","011","016","02","031"};
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_dropdown_item, items);
+
+        spinner.setAdapter(adapter);
+
+
+        Spinner spinner2 = findViewById(R.id.spinner2);
+
+        String[] items2 = new String[]{"220","225","230","235","240","245","250","255","260","265","270"};
+
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_dropdown_item, items2);
+
+        spinner2.setAdapter(adapter2);
+
+        Spinner spinner3 = findViewById(R.id.spinner3);
+
+        String[] items3 = new String[]{"30","40","50","60","70","80","90","100","110","120"};
+
+
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_dropdown_item, items3);
+
+        spinner3.setAdapter(adapter3);
+
+        Spinner spinner4 = findViewById(R.id.spinner4);
+
+        String[] items4 = new String[]{"130","140","150","160","170","180","190","200"};
+
+
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_dropdown_item, items4);
+
+        spinner4.setAdapter(adapter4);
+
+
+
+
+
+
+
+
+
+
+
+
+
         /*
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
